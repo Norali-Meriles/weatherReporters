fbdf618c87f37f7a4f7b1b173c5ef2599c2cde00
Norali Meriles <merilesnorali.1996@gmail.com>
2022-09-22 18:34:05 -0300

    estructura card

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { WeatherCard } from '../components/WeatherCard';
-import { Loading } from '../components/Loading';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { WeatherCard } from "../components/WeatherCard";
+import { Loading } from "../components/Loading";
+import { Card } from "../components/Card";
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -9,7 +10,9 @@ const HomePage = () => {
 
   const getCurrentWeather = async () => {
     setIsLoading(true);
-    const { data } = await axios('http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=auto:ip');
+    const { data } = await axios(
+      "http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=auto:ip"
+    );
     setWeather(data);
     setIsLoading(false);
   };
@@ -22,7 +25,14 @@ const HomePage = () => {
 
   return (
     <div>
-        {isLoading ? <Loading /> : <WeatherCard weather={weather} />}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <WeatherCard weather={weather} />
+          <Card className={"col-5 col-lg-2 "} weather={weather} />
+        </>
+      )}
     </div>
   );
 };
