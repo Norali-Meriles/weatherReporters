4fe0c1d9d20e5ff98a9df2fcaba447e47ab40c57
Marcelo Acevedo <miqueasacevedo2017@gmail.com>
2022-09-30 14:22:49 -0300

    add custom hook for fetch

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { WeatherCard } from '../components/WeatherCard';
 import { Loading } from '../components/Loading';
+import useFetch from '../hooks/CustomFetch/useFetch';
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -9,7 +9,7 @@ const HomePage = () => {
 
   const getCurrentWeather = async () => {
     setIsLoading(true);
-    const { data } = await axios('http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=auto:ip');
+    const { data } = await useFetch('http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=auto:ip');
     setWeather(data);
     setIsLoading(false);
   };
@@ -17,9 +17,6 @@ const HomePage = () => {
   useEffect(() => {
     getCurrentWeather();
   }, []);
-
-  console.log(weather);
-
   return (
     <div>
         {isLoading ? <Loading /> : <WeatherCard weather={weather} />}
