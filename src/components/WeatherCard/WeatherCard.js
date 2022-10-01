d548188dbe49510282f9b48fce6788a8fd6392d2
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-10-01 12:12:50 -0300

    hice container-fluid HomePage

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 import { useClimaContex } from '../../providers/ClimaProvider';
 import { useTheme } from '../../providers/ThemeProvider';
 
@@ -8,18 +8,32 @@ const WeatherCard = ({ weather }) => {
   const { theme } = useTheme();
 
   return (
-    <div className="card" style={{ backgroundColor: theme.background, color: theme.textColor }}>
+    <div
+      className='card'
+      style={{ backgroundColor: theme.background, color: theme.textColor }}
+    >
       <img
-        src={data ? data.current?.condition.icon : weather.current?.condition.icon}
-        className="card-img-top"
-        alt="condition icon"
+        src={
+          data ? data.current?.condition.icon : weather.current?.condition.icon
+        }
+        className='card-img-top'
+        alt='condition icon'
       />
-      <div className="card-body">
-        <h2 className="card-title">{data ? data.current?.feelslike_c : weather.current?.feelslike_c}ºC</h2>
-        <h5 className="card-text">{data ? data.current?.condition.text : weather.current?.condition.text}</h5>
-        <p>{data ? data.current?.last_updated : weather.current?.last_updated}</p>
+      <div className='card-body'>
+        <h2 className='card-title'>
+          {data ? data.current?.feelslike_c : weather.current?.feelslike_c}ºC
+        </h2>
+        <h5 className='card-text'>
+          {data
+            ? data.current?.condition.text
+            : weather.current?.condition.text}
+        </h5>
         <p>
-          <i className="bi bi-geo-alt-fill"></i> {data ? data.location?.tz_id : weather.location?.tz_id} - {' '}
+          {data ? data.current?.last_updated : weather.current?.last_updated}
+        </p>
+        <p>
+          <i className='bi bi-geo-alt-fill'></i>{' '}
+          {data ? data.location?.tz_id : weather.location?.tz_id} -{' '}
           {data ? data.location?.name : weather.location?.name}
         </p>
       </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -25,7 +25,7 @@ const HomePage = () => {
 
   return (
     <ClimaProvider>
-    <div className="container" style={{ backgroundColor: theme.background, color: theme.textColor }}>
+    <div className="container-fluid" style={{ backgroundColor: theme.background, color: theme.textColor }}>
       <div className="row">
         <div className="col-6">
         <Search />
