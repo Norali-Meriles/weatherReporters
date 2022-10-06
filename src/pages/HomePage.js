e9a95f8cfceb22c8888a69d54d695d367d3ad550
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-10-05 22:15:12 -0300

    Hook axios (#7)
    * add custom hook for fetch
    
    * fixed merge error
    
    * hook fetch para consultar url
    
    * fix changes a solicitud de ceci

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useTheme } from '../providers/ThemeProvider';
 import { WeatherCard } from '../components/WeatherCard';
 import { Loading } from '../components/Loading';
+import useFetch from '../hooks/CustomFetch/useFetch';
 import { Search } from '../components/Search';
 import { ClimaProvider } from '../providers/ClimaProvider';
 import InfoCards from '../components/InfoCards/InfoCards';
-import { useTheme } from '../providers/ThemeProvider';
 
 const HomePage = () => {
-  const { theme } = useTheme();
   const [isLoading, setIsLoading] = useState(false);
   const [weather, setWeather] = useState([]);
-  const { theme } = useTheme();
 
   const getCurrentWeather = async () => {
     setIsLoading(true);
-    const { data } = await axios(
-      'http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=auto:ip',
-    );
+    const { data } = await useFetch('http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=auto:ip');
     setWeather(data);
     setIsLoading(false);
   };
@@ -26,17 +20,22 @@ const HomePage = () => {
   useEffect(() => {
     getCurrentWeather();
   }, []);
-
   return (
     <ClimaProvider>
-    <div className="container" style={{ backgroundColor: theme.background, color: theme.textColor }}>
+    <div className="container">
       <div className="row">
-        <div className="col col-md-6 col-sm-12">
+        <div className="col-6">
         <Search />
         {isLoading ? <Loading /> : <WeatherCard weather={weather} />}
         </div>
-        <div className="col col-md-6 col-sm-12 my-5">
-          {isLoading ? <Loading /> : <InfoCards weather={weather} />}
+        <div className="col-6">
+          <div>aca va componente de Norali</div>
+          <br />
+          <br />
+          <br />
+          <div>
+            <InfoCards />
+          </div>
         </div>
       </div>
     </div>
