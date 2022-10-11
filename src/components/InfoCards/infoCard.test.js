2c5a05fc828ae6c370af95aa8671bd817a71deab
Norali Meriles <merilesnorali.1996@gmail.com>
2022-10-11 09:53:24 -0300

    detalles descriptivos  de test

--- src/components/InfoCards/infoCard.test.js
+++ src/components/InfoCards/infoCard.test.js
@@ -4,7 +4,7 @@ import InfoCards from './InfoCards';
 import { weatherInfoCard } from '../../utils/test/mock-data';
 
 describe('card InfoCard', () => {
-  test('Esperamos el render de forecast', () => {
+  test('Esperamos el render de InfoCard', () => {
     render(<ClimaProvider ><InfoCards weather={weatherInfoCard}/> </ClimaProvider>);
     const resultado = screen.getByText('10 km');
     expect(resultado).toBeInTheDocument();
--- src/components/Theme/theme.test.js
+++ src/components/Theme/theme.test.js
@@ -2,8 +2,8 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { ThemeProvider } from '../../providers/ThemeProvider';
 import Theme from './Theme';
 
-describe('card InfoCard', () => {
-  test('Esperamos el render de forecast', () => {
+describe('card Theme', () => {
+  test('Esperamos el cambio de estilo', () => {
     render(<ThemeProvider><Theme/> </ThemeProvider>);
     const switchElement = screen.queryByRole('switch');
     fireEvent.click(switchElement);
